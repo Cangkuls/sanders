494799347e47ba59814663c7ab8a6aef67b37a27
YudiWidiyanto <yudiwidiyanto7@gmail.com>
2018-04-18 12:00:21 +0700

    Add build tools
    * anykernel KANG from https://github.com/AlizeKernel/AnyKernel2/tree/sanders-eas
    
    Signed-off-by: YudiWidiyanto <yudiwidiyanto7@gmail.com>

---
+++ AnyKernel2/anykernel.sh
@@ -0,0 +1,54 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string= osm0sis @ xda-developers
+do.devicecheck=0
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+
+## AnyKernel install
+# alert of unsupported Android version
+android_ver=$(grep "^ro.build.version.release" /system/build.prop | cut -d= -f2);
+case "$android_ver" in
+  "8.0.0"|"8.1.0") support_status="supported";;
+  *) support_status="unsupported";;
+esac;
+ui_print " ";
+ui_print "Running Android $android_ver..."
+ui_print "This kernel is $support_status for this version!";
+
+dump_boot;
+
+# Set the default background app limit to 60
+insert_line default.prop "ro.sys.fw.bg_apps_limit=60" before "ro.secure=1" "ro.sys.fw.bg_apps_limit=60";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
+
